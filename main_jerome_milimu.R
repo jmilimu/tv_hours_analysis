152373eab8bf325815f320792c3918fd0285a03c
Jerome-Milimu <83632688+jmilimu@users.noreply.github.com>
2024-04-12 22:40:04 +0200

    Filter to under 30

--- main_jerome_milimu.R
+++ main_jerome_milimu.R
@@ -4,7 +4,7 @@ library(here)
 
 tv_hours_table <-  
   gss_cat %>% 
-  filter(age >= 30) %>% 
+  filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
